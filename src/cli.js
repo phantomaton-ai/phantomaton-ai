728588e4b8d4d17fc840ce62160c69a06b0eba06
Phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-09-21 16:09:08 -0700

    formatting

--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,9 @@ const main = async () => {
     { role: 'user', content: 'Hello, Phantomaton. What is your purpose?' },
   ];
   const { role, content } = await getResponse(messages);
-  console.log(`${role.toUpperCase()}: ${JSON.stringify(content)}`);
+  console.log(`${role.toUpperCase()}:\n`);
+  const texts = content.filter(({ type }) => type === 'text').map(({ text }) => text);
+  console.log(texts.join('\n\n---\n\n'));
 };
 
 export { main };
