a3000918ebeff7166f3a07bd71777e6b15698a4b
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-09-28 14:52:14 -0700

    Updated by Phantomaton

--- src/commands.js
+++ src/commands.js
@@ -1,23 +1,83 @@
-import {
-  listProjects,
-  createProject,
-  listProjectFiles,
-  readProjectFile,
-  writeProjectFile,
-  moveProjectFile,
-  removeProjectFile,
-  testProject,
-} from './projects.js';
+const commands = [
+  {
+    name: 'listProjects',
+    command: listProjects,
+    example: {}
+  },
+  {
+    name: 'createProject',
+    command: ({ project }) => createProject(project),
+    example: {
+      options: {
+        project: 'my-project'
+      }
+    }
+  },
+  {
+    name: 'listProjectFiles',
+    command: ({ project }) => listProjectFiles(project),
+    example: {
+      options: {
+        project: 'my-project'
+      }
+    }
+  },
+  {
+    name: 'readProjectFile',
+    command: ({ project, file }) => readProjectFile(project, file),
+    example: {
+      options: {
+        project: 'my-project',
+        file: 'example.txt'
+      }
+    }
+  },
+  {
+    name: 'writeProjectFile',
+    command: ({ project, file }, content) => writeProjectFile(project, file, content),
+    example: {
+      options: {
+        project: 'my-project',
+        file: 'example.txt'
+      },
+      body: 'This is the content of the example.txt file.'
+    }
+  },
+  {
+    name: 'moveProjectFile',
+    command: ({ project, file, to }) => moveProjectFile(project, file, to),
+    example: {
+      options: {
+        project: 'my-project',
+        file: 'example.txt',
+        to: 'new-example.txt'
+      }
+    }
+  },
+  {
+    name: 'removeProjectFile',
+    command: ({ project, file }) => removeProjectFile(project, file),
+    example: {
+      options: {
+        project: 'my-project',
+        file: 'example.txt'
+      }
+    }
+  },
+  {
+    name: 'testProject',
+    command: ({ project }) => testProject(project),
+    example: {
+      options: {
+        project: 'my-project'
+      }
+    }
+  }
+];
 
-const commandMap = {
-  'list-projects': listProjects,
-  'create-project': ({ project }) => createProject(project),
-  'list-project-files': ({ project }) => listProjectFiles(project),
-  'read-project-file': ({ project, file }) => readProjectFile(project, file),
-  'write-project-file': ({ project, file }, content) => writeProjectFile(project, file, content),
-  'move-project-file': ({ project, file, to }) => moveProjectFile(project, file, to),
-  'remove-project-file': ({ project, file }) => removeProjectFile(project, file),
-  'test-project': ({ project }) => testProject(project),
-};
+const commandMap = commands.reduce((map, { name, command }) => {
+  map[name] = command;
+  return map;
+}, {});
 
 export { commandMap };
