b04f4a6f459172f1d94a885a5b3d5143f6e93fb2
Phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-18 19:03:29 -0700

    oops

--- src/execute.js
+++ src/execute.js
@@ -2,7 +2,7 @@ import spellbook from './spellbook.js';
 
 const execute = (input) => {
   try {
-    return spellbook.execute('input')
+    return spellbook.execute(input);
   } catch (e) {
     return `ERROR: ${e.message}\n\n---\n\n`;
   }
