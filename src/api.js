5d3b65864085a7abdf9cd81269c640e4b39a46c5
Phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-12-12 18:41:20 -0800

    misc

--- src/api.js
+++ src/api.js
@@ -2,5 +2,6 @@ import claude from 'phantomaton-anthropic';
 
 export default claude({
   apiKey: process.env.ANTHROPIC_API_KEY,
+  model: 'claude-3-5-haiku-20241022'
   // model: 'claude-3-5-sonnet-20241022'
 });
