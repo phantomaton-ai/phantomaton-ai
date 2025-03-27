01b082c3b11fdab0ca1d77bd5d9f80cc26861d4c
Phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-27 14:36:41 -0700

    interrupt

--- src/chat.js
+++ src/chat.js
@@ -1,23 +1,18 @@
 import chalk from 'chalk';
 import { Conversation } from './conversation.js';
 import { getUserInput } from './input.js';
+import interrupt from './interrupt.js';
 
 const output = text => process.stdout.write(text);
 const main = async (conversation) => {
   while (true) {
-    if (conversation.preamble.trim().length < 1) {
-      const userInput = await getUserInput('> ');
-      if (userInput.toLowerCase() === 'exit') break;
-      output(`\n${chalk.blue(conversation.prompt)}\n\n`);
-      const { response, preamble } = await conversation.advance(userInput);
-      output(`\n${chalk.green(response)}\n\n`);
-      if (preamble.length > 0) output(`\n\n${chalk.magenta(preamble)}\n`);
-    } else {
-      await new Promise(res => setTimeout(res, 250));
-      const { response, preamble } = await conversation.advance('');
-      output(`\n${chalk.green(response)}\n\n`);
-      if (preamble.length > 0) output(`\n\n${chalk.magenta(preamble)}\n`);
-    }
+    const inputNeeded = (conversation.preamble.trim().length < 1) || await interrupt(5000);
+    const userInput = inputNeeded ? await getUserInput('> ') : '';
+    if (userInput.toLowerCase() === 'exit') break;
+    output(`\n${chalk.blue(conversation.prompt)}\n\n`);
+    const { response, preamble } = await conversation.advance(userInput);
+    output(`\n${chalk.green(response)}\n\n`);
+    if (preamble.length > 0) output(`\n\n${chalk.magenta(preamble)}\n`);
   }
 
   output('Farewell, foolish humans! 🤖\n');
