9553d82eaa1bc7b54d3399debe565063aefdaf14
Phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-27 14:39:39 -0700

    gemini

--- src/api.js
+++ src/api.js
@@ -1,9 +1,16 @@
-import claude from 'phantomaton-anthropic';
+// import claude from 'phantomaton-anthropic';
+//
+// export default claude({
+//   apiKey: process.env.ANTHROPIC_API_KEY,
+//   // model: 'claude-3-7-sonnet-20250219'
+//   model: 'claude-3-5-haiku-20241022'
+//   // model: 'claude-3-5-sonnet-20241022'
+//   // model: 'claude-3-haiku-20240307'
+// });
 
-export default claude({
-  apiKey: process.env.ANTHROPIC_API_KEY,
-  // model: 'claude-3-7-sonnet-20250219'
-  model: 'claude-3-5-haiku-20241022'
-  // model: 'claude-3-5-sonnet-20241022'
-  // model: 'claude-3-haiku-20240307'
+import gemini from 'phantomaton-gemini';
+
+export default gemini({
+  apiKey: process.env.GEMINI_API_KEY,
+  model: 'gemini-2.0-flash'
 });
