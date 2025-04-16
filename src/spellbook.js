2c7650c03784746c3db79f1c790bfe92b22c9c7a
Doc Woe <woe@phantomaton.com>
2025-04-16 21:02:43 +0000

    includes

--- src/spellbook.js
+++ src/spellbook.js
@@ -3,5 +3,6 @@ import projects from 'phantomaton-projects';
 
 const { commands } = projects({ home: 'data/projects' });
 const symbols =  { directive: { start: '🪄✨ ', end: '⚡️' } };
+const includes = { text: false };
 
-export default necronomicon({ commands, symbols });
+export default necronomicon({ commands, symbols, includes });
