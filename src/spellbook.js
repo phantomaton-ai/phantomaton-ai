50a0e5817d7ace3e05bf6c2b992dcff2af9b7eb8
Phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-05-22 13:46:02 -0700

    whitespace

--- src/spellbook.js
+++ src/spellbook.js
@@ -1,8 +1,8 @@
 import necronomicon from 'necronomicon';
 import projects from 'phantomaton-projects';
 
-const { commands } = projects({ home: 'data/projects' });
-const symbols =  { directive: { start: '🪄✨ ', end: '⚡️' } };
+const { commands } = projects({ home: '~/.phantomaton/projects' });
+const symbols = { directive: { start: '🪄✨ ', end: '⚡️' } };
 const includes = { text: false };
 
 export default necronomicon({ commands, symbols, includes });
