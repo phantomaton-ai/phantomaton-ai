e514ce85538455e9e46bf949cc14343ee83b063d
Phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-05-22 14:37:38 -0700

    oops

--- src/spellbook.js
+++ src/spellbook.js
@@ -1,7 +1,7 @@
 import necronomicon from 'necronomicon';
 import projects from 'phantomaton-projects';
 
-const { commands } = projects({ home: '~/.phantomaton/projects' });
+const { commands } = projects({ home: 'data/projects' });
 const symbols = { directive: { start: '🪄✨ ', end: '⚡️' } };
 const includes = { text: false };
 
